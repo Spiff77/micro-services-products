f5ce701357a7e50aa364e21071fe725c8fa666cf
Thomas Lhomme <blabla@blabla.com>
2020-06-22 09:45:12 +0200

    Cart Service

--- cart-service/src/main/java/com/demomicro/cartservice/controller/CartController.java
+++ cart-service/src/main/java/com/demomicro/cartservice/controller/CartController.java
@@ -1,27 +1,40 @@
 package com.demomicro.cartservice.controller;
 
-import com.microdemo.productservice.model.Product;
+import com.demomicro.cartservice.model.Categorie;
+import com.demomicro.cartservice.model.Product;
+import com.demomicro.cartservice.model.ProductItem;
+import com.demomicro.cartservice.model.ProductList;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.client.RestTemplate;
 
+import java.util.ArrayList;
 import java.util.Arrays;
 import java.util.List;
 
-public class ProductController {
+@RestController
+public class CartController {
 
+    RestTemplate restTemplate = new RestTemplate();
 
-    @GetMapping("products/{userid}")
-    public List<Product> getAll(@PathVariable("userid") int userid) {
-        return Arrays.asList(
-                new Product(1, "Product 1" , 3),
-                new Product(2, "Product 2", 1),
-                new Product(3, "Product 3", 2)
-        );
+    @GetMapping("carts/{userid}")
+    public List<ProductItem> getAll(@PathVariable("userid") int userid) {
+        List<ProductItem> productItems = new ArrayList<>();
+
+        restTemplate.getForObject("http://localhost:8082/products/user/"+userid, ProductList.class)
+                .getProducts().stream().forEach( p -> {
+                    ProductItem pi = new ProductItem();
+                    pi.setId(p.getId());
+                    pi.setProduct_name(p.getName());
+
+                    Categorie c = restTemplate.getForObject("http://localhost:8081/categories/"+p.getCategorie_id(), Categorie.class);
+                    pi.setCategory_name(c.getName());
+                    productItems.add(pi);
+                });
+
+        return productItems;
     }
 
-    @GetMapping("products/{id}")
-    public Product getOne(@PathVariable("id") int id) {
-        return new Product(id, "Product ", 2);
 
-    }
 }
--- cart-service/src/main/java/com/demomicro/cartservice/model/Categorie.java
+++ cart-service/src/main/java/com/demomicro/cartservice/model/Categorie.java
@@ -1,10 +1,12 @@
-package com.demomicro.categorieservice.model;
+package com.demomicro.cartservice.model;
 
 import lombok.AllArgsConstructor;
 import lombok.Data;
+import lombok.NoArgsConstructor;
 
 @Data
 @AllArgsConstructor
+@NoArgsConstructor
 public class Categorie {
 
     private int id;
